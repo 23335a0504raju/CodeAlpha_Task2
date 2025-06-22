fb33180de3b27819a110767079ed70448101a74a
23335a0504raju <smilyraju8464@gmail.com>
2025-06-22 17:18:50 +0530

    updated urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,9 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
 
+def health_check(request):
+    return HttpResponse("Server is running", status=200)
 
 urlpatterns = [
     path("api/",include("connect.urls")),
     path("admin/", admin.site.urls),
+    path('', health_check),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
